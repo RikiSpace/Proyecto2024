0a860d7d7b2d27dbe0287b41cc932545d71ad91b
RikiSpace <lopez.rikelmer@iemodeliaibague.edu.co>
2024-06-04 05:50:24 -0500

    Segunda version proyecto, captura grado y muestra en forma de lista

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int nEdad, sumaP=0, contador5=0, sumaN=0, promedioP=0, promedioN=0, contadorN=0, contadorP=0;
-        System.out.println("Por favor ingrese la cantidad de edades que va a ingresar");
+        int nEdad,sumaP=0, contador5=0, sumaN=0, promedioP=0, promedioN=0, contadorN=0, contadorP=0, grado[];
+
+        System.out.println("Por favor ingrese la cantidad de grados y edades que va a ingresar");
         nEdad = entrada.nextInt();
 
-        int edad[] = new int[nEdad];
+        grado = new int[nEdad];
+        for (int i = 0; i < nEdad; i++) {
+            System.out.println("Por favor digite los grados");
+            grado[i] = entrada.nextInt();
 
+        }
+        int edad[] = new int[nEdad];
         for (int i = 0; i < nEdad; i++) {
             System.out.println("Por favor digite las edades");
             edad[i] = entrada.nextInt();
-            if (edad[i]==5){
-                contador5 ++;
-            }
-            else if (edad[i]>5){
+
+            /*if (edad[i] == 5) {
+                contador5++;
+            } else if (edad[i] > 5) {
                 contadorP++;
                 sumaP += edad[i];
-            }
-            else  {
+            } else {
                 contadorN++;
                 sumaN += edad[i];
-            }
+            }*/
         }
+        //int mostrar[] = new int[nEdad];
+        int i=0;
+        for (int j=0;j<nEdad;j++);{
+            System.out.println((i+1)+"La edad es: "+edad[i]+" - El grado es: "+grado[i]);
+
+        }
+
+        /*
         if (contadorP==0){
             System.out.println("No se puede hacer el promdeio de edades mayores a 5 años");
         }
@@ -39,12 +52,24 @@ public class Main {
             System.out.println("\nEl promdeio de las edades menores a 5 años es: "+promedioN);
         }
         System.out.println("\n Las edades son: ");
+
+
         for (int i=0;i<nEdad;i++){
-            System.out.println(edad[i] + " ");
+            mostrar[j]=grado[i];
+            j++;
+            mostrar[j]=edad[i];
+            j++;
+            System.out.println(edad[i] + " ");+
 
-        }
-        System.out.println("\nLa cantidad de estudiantes que tienen 5 años es:"+contador5);
+            System.out.println("\nLa cantidad de estudiantes que tienen 5 años es:"+contador5);*/
 
+       /* int mostrar[] = new int[nEdad];
+        int i=0;
+
+        for (int j=0;j<nEdad*2;j++);{
+            System.out.println("La edad es: "+edad[i]+" - El grado es: "+grado[i]);
+
+        }*/
 
     }
 }
